807c2abed8acdd63b74529224452c9b2f5786d11
p.tychinin <catgirlfighter@gmail.com>
2025-07-20 19:40:57 +0300

    ﻿1.2.2

--- Source/ResearchWhatever16/Patches/StudyManagerPatch.cs
+++ Source/ResearchWhatever16/Patches/StudyManagerPatch.cs
@@ -16,6 +16,13 @@ namespace ResearchWhatever.Patches
     static class StudyManager_StudyAnomaly_ResearchWhateverPatch
     {
         //private static readonly PropertyInfo LStudyCompleted = AccessTools.Property(typeof(ITab_StudyNotes), "StudyCompleted");
+
+        internal static bool CheckForBullshit(Thing studiedThing)
+        {
+            return (!(studiedThing is UnnaturalCorpse)) || (((UnnaturalCorpse)studiedThing).Tracker.CanDestroyViaResearch);
+            //return !(studiedThing UnnaturalCorpse) ((UnnaturalCorpse)studiedThing).tarcker
+        }
+
         internal static void Prefix(Thing studiedThing, Pawn studier, float knowledgeAmount, KnowledgeCategoryDef knowledgeCategory)
         {
             if (!ModsConfig.AnomalyActive)
@@ -47,28 +54,38 @@ namespace ResearchWhatever.Patches
             {
                 var compStudiable = studiedThing.TryGetComp<CompStudiable>();
                 CompHoldingPlatformTarget compHoldingPlatformTarget = studiedThing.TryGetComp<CompHoldingPlatformTarget>();
-                bool b = false;
-                if (compStudiable != null)
+                bool unlockComplete = false;
+                bool studiableComplete = false;
+                bool buillshitComplete = CheckForBullshit(studiedThing);
+                //bool tabComplete = false;
+                //I'm so pissed, why the fuck research progress has so many channels, and only fucking one that unites them ALL is a fucking TAB??? (ITab_StudyNotes) WHAT THE FUCK???
+                //What the fuck am I supposed to do to take data from that scuffed thing without VISUALLY selecting shit? Fuck me!
+                //var tabType = studiedThing.def.inspectorTabs.FirstOrDefault(x => x.SameOrSubclassOf<ITab_StudyNotes>());
+
+                var compStudyUnlocks = studiedThing.TryGetComp<CompStudyUnlocks>();
+                //Log.Message($"unlocks = {compStudyUnlocks}, completed = {compStudyUnlocks?.Completed}");
+                if (compStudyUnlocks == null || compStudyUnlocks.Completed)
                 {
-                    if (compStudiable.Props.Completable)
-                        if (compStudiable.Completed)
-                        {
-                            compStudiable.studyEnabled = false;
-                            b = true;
-                        }
-                        else { }
-                    else
-                    {
-                        var compStudyUnlocks = studiedThing.TryGetComp<CompStudyUnlocks>();
-                        if (compStudyUnlocks == null || compStudyUnlocks.Completed)
-                        {
-                            compStudiable.studyEnabled = false;
-                            b = true;
-                        }
-                    }
+                    //compStudiable.studyEnabled = false;
+                    unlockComplete = true;
                 }
 
-                if (b)
+                if (compStudiable != null)
+                {
+                    //Log.Message($"studiable, {compStudiable.studyPoints}/{compStudiable.Props.studyAmountToComplete} completable = {compStudiable.Props.Completable}, completed = {compStudiable.Completed}");
+                    if (!compStudiable.Props.Completable || compStudiable.Completed)
+                        {
+                        //compStudiable.studyEnabled = false;
+                        studiableComplete = true;
+                        }
+                    //    else { }
+                    //else
+                    //{
+                    //}
+                    if (buillshitComplete && unlockComplete && studiableComplete) compStudiable.studyEnabled = false;
+                }
+
+                if (buillshitComplete && unlockComplete && studiableComplete)
                 {
                     if (compHoldingPlatformTarget?.containmentMode == EntityContainmentMode.Study) compHoldingPlatformTarget.containmentMode = EntityContainmentMode.MaintainOnly;
                     Messages.Message("ResearchWhateverNothingLeftToResearch".Translate(studiedThing.Label).CapitalizeFirst(), new TargetInfo(studiedThing.PositionHeld, studiedThing.MapHeld, false), MessageTypeDefOf.NeutralEvent);
